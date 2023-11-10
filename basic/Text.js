802d0642e17981ed86e0398890fa1ec10cd419dd
dev4ajay <128267062+dev4ajay@users.noreply.github.com>
2023-11-10 05:39:49 +0000

    number

--- basic/Text.js
+++ basic/Text.js
@@ -3,7 +3,58 @@ var Name = "Ajay";
 let Email = "Ajay@gmail.com";
 Name = "Kummar";
 Email = "Kumar@gmail.com";
-Password = 12121212;
+// Password = 12121212;
 
 
 console.log(Name);
+
+
+///primitiveData //
+
+// number 
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol  
+
+
+console.table([typeof "Ajay" , typeof 123 , typeof null , typeof undefined , typeof true]);
+
+// let A = 33
+
+// console.log(typeof A);
+
+// let B = "33";
+
+// console.log(typeof B);
+
+// let C = "33abc";
+
+// console.log(typeof C);
+
+
+// let D = true
+
+// console.log(typeof D);
+
+// let store = 1;
+
+// let login = Boolean(store);
+
+// console.log(login);
+
+// let M = "33abc";
+
+// let X = Number(M);
+
+// console.table([typeof X , X]);
+
+// let N = "33";
+
+// let Y = Number(N);
+
+// console.table([typeof Y , Y]);
+
+
